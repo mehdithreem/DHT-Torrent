b43af2e3b8a45565eaead8f3a5d80a6b0bd68667
Nahal Mirzaie <nahal@Nahals-MacBook-Pro.local>
2017-04-04 16:59:51 +0430

    socket updated

--- socket.py
+++ socket.py
@@ -1,11 +1,11 @@
 import socket, select
-import json
-
+import json 
 
 class Socket:
-    def __init__(self, port=12345, numberOfSender=4, IP="127.0.0.1", bufferSize=4096):
+
+    def __init__ (self ,port= 12345 ,numberOfSender=4 ,IP="127.0.0.1" ,bufferSize = 4096):
         self.READER_LIST = []
-        self.WRITER_LIST = []
+        self.WRITER_LIST = []       
         self.RECV_BUFFER = bufferSize
         self.PORT = port
 
@@ -13,5 +13,29 @@ class Socket:
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((IP, self.PORT))
         self.server_socket.listen(numberOfSender)
-
+ 
         self.READER_LIST.append(server_socket)
+
+    def run(self):
+        while True:
+            read_sockets,write_sockets,error_sockets = select.select(self.READER_LIST,self.WRITER_LIST,[])
+            
+            for sock in write_sockets:
+                pass #TODO
+
+            for sock in read_sockets:
+                pass #TODO
+            
+        self.server_socket.close()
+
+    def addToReadList(self):
+        pass
+
+    def addToWriteList(self):
+        pass
+
+    def removeFromReadList(self):
+        pass
+
+    def removeFromWriteList(self):
+        pass
